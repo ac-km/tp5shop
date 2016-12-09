c9dff62d09152c13b349454942cd95ded08c0a4a
puzhongbo <804259513@qq.com>
2016-12-09 14:17:16 +0800

    'ThinkPHP手册阅读及对应练习';

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -9,11 +9,27 @@ namespace app\index\controller;
 use think\Controller;
 use app\index\model\User as UserModel;
 class User extends Controller{
+    public function index(){
+//        $list = UserModel::all();
+//        $list = UserModel::where('id','<',10)->select();
+        $list = UserModel::all();
+        foreach ($list as $user){
+            echo $user->id . '<br>';
+            echo $user->nickname . '<br>';
+            echo $user->email . '<br>';
+            echo $user->birthday . '<br>';
+            echo $user->create_time . '<br>';
+            echo $user->update_time . '<br>';
+            echo $user->status . '<br>';
+            echo '<hr>';
+        }
+
+    }
     public function add(){
 
         //方式1
         /*
-        $user = new UserModel;
+        $user = new UserModel();
         $user -> nickname = '流年';
         $user -> email = 'THINKPHP@qq.com';
         $user -> birthday = strtotime('1997-03-05');
@@ -23,31 +39,91 @@ class User extends Controller{
             return $user -> getError();
         }*/
         //方式2
-        $user['nickname'] = '看云';
-        $user['email'] = 'kancloud@qq.com';
-        $user['birthday'] = strtotime('2015-04-02');
+        $user['nickname'] = '流年';
+        $user['email'] = 'test1@qq.com';
+        $user['birthday'] = '1995-04-02';
         if($result = UserModel::create($user)){
             return '用户[' . $result['nickname'] . ':' .$result->id . ']新增成功';
         }else{
             return '新增出错';
         }
     }
-    public function index(){
-        echo 'index';
-    }
     public function create(){
         echo 'create';
     }
     public function addList(){
-        echo 'add_list';
+        $user = new UserModel();
+        $list = [
+            ['nickname'=>'张三','email'=>'zhangsan@qq.com','birthday'=>strtotime('1988-01-15')],
+            ['nickname'=>'李四','email'=>'lisi@qq.com','birthday'=>strtotime('1990-09-19')],
+        ];
+        if($user->saveAll($list)){
+            return '用户批量新增成功';
+        }else{
+            return $user->getError();
+        }
+
     }
-    public function update(){
-        echo 'update';
+    public function update($id){
+        /*$user = UserModel::get($id);
+        if(isset($user->id)){
+            //更新数据
+            $user->nickname = '波波';
+            $user->email = '804259513@qq.com';
+            $user->birthday = strtotime('1990-10-18');
+            if($user->save()){
+                echo '更新成功' . '<br>';
+                echo $user->id . '<br>';
+                echo $user->nickname . '<br>';
+                echo $user->email . '<br>';
+                echo date('Y/m/d',$user->birthday);
+            }else{
+                echo '更新数据失败';
+            }
+        }else{
+            //不存在该用户
+            echo '不存在该用户';
+        }*/
+        $user['id'] = (int)$id;
+        $user['nickname'] = '刘程';
+        $user['email'] = 'liu21st@gmail.com';
+        $result = UserModel::update($user);
+        return '更新用户成功' . $result['nickname'];
+
     }
-    public function delete(){
-        echo 'delete';
+    public function delete($id){
+        /*$user = UserModel::get($id);
+        if($user){
+            $user->delete();
+            return '删除用户成功';
+        }else{
+            return '删除的用户不存在';
+        }*/
+        $result = UserModel::destroy($id);
+        if($result){
+            return '删除用户成功';
+        }else{
+            return '删除的用户不存在';
+        }
     }
-    public function read(){
-        echo 'read';
+    public function read($id=''){
+        $user = UserModel::get($id);
+        /*if(isset($user->id)){
+            echo $user->nickname . '<br>';
+            echo $user->email . '<br>';
+            echo date('Y/m/d',$user->birthday) . '<br>';
+        }else{
+            echo '该用户不存在';
+        }*/
+//        $user = UserModel::getByEmail("thinkphp@qq.com");
+//        $user = UserModel::get(['nickname'=>'看云']);
+//        $user = UserModel::where(['nickname'=>'张三'])->find();
+        echo $user['id'] . '<br>';
+        echo $user['nickname'] . '<br>';
+        echo $user['email'] . '<br>';
+        echo $user['birthday'] . '<br>';
+        echo $user['create_time'] . '<br>';
+        echo $user['status'] . '<br>';
+//        echo $user['user_birthday'] . '<br>';
     }
 }
--- application/index/model/User.php
+++ application/index/model/User.php
@@ -8,5 +8,42 @@
 namespace app\index\model;
 use think\Model;
 class User extends Model{
+    //读取器1
+//    protected function getBirthdayAttr($birthday){
+//        return date('Y-m-d',$birthday);
+//    }
+    //读取器2
+//    protected function getUserBirthdayAttr($value,$data){
+//        return date('Y-m-d',$data['birthday']);
+//    }
+//    //修改器
+//    protected function setBirthdayAttr($value){
+//        return strtotime($value);
+//    }
+      protected $type = [
+          //设置birthday为时间戳类型(整型)
+          'birthday' => 'timestamp:Y-m-d H:i:s',
+          'create_time' => 'timestamp',
+          'update_time' => 'timestamp',
+      ];
+    //自动完成
+    protected $auto = ['status'];
+    //修改器
+    protected function setStatusAttr($value,$data){
+        return '流年' == $data['nickname'] ? 1 : 2 ;
+    }
+    //读取器
+    protected function getStatusAttr($value){
+        $status = [-1 => '删除' , 0 => '禁用' , 1 => '正常' , 2 => '待审核'];
+        return $status[$value];
+    }
+    //查询范围
+    protected function scopeEmail($query){
+        $query -> where('email','804259513@qq.com');
+    }
+    //全局查询范围
+    protected  static function base($query){
+        $query -> where('status=1');
+    }
 
 }
